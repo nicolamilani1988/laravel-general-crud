21cc5794d4340cbe8b0f10ad5516b0db151e9296
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-05-28 16:07:54 +0200

    esercizio ok

--- test_CRUD_consolidamento/public/js/app.js
+++ test_CRUD_consolidamento/public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\BOOLEAN\laravel\test_CRUD_consolidamento\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\BOOLEAN\laravel\test_CRUD_consolidamento\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\BOOLEAN\1-Github\laravel-general-crud\test_CRUD_consolidamento\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\BOOLEAN\1-Github\laravel-general-crud\test_CRUD_consolidamento\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
